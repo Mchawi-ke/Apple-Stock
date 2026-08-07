f5bebfaf92c8a00b9a7045576b3480dddbb62c56
Mchawi-ke <cmusava6@gmail.com>
2026-08-07 20:50:21 +0300

    first commit

--- main.py
+++ main.py
@@ -1,1 +1,17 @@
+# pip install numpy pandas matplotlib plotly statsmodels scikit-learn
+
+
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import plotly.express as px
+from plotly.subplots import make_subplots
+import plotly.graph_objects as go
+from statsmodels.tsa.seasonal import seasonal_decompose
+from statsmodels.tsa.stattools import adfuller
+from statsmodels.graphics.tsaplots import plot_acf, plot_pacf
+import statsmodels.api as sm
+from sklearn.metrics import mean_absolute_error, mean_absolute_percentage_error
+
+
 
